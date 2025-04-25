153d5129b0ba6260afaeea06cd1874c8d7a3078e
Pecet13 <pecet113@gmail.com>
2025-04-25 02:30:21 +0200

    Added more serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Product, Category, Comparison, Ranking
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +12,32 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'image_url']
+        extra_kwargs = {'image_url': {'required': False}}
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class ComparisonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comparison
+        fields = ['id', 'user', 'category', 'product1', 'product2', 'result', 'user_created']
+        extra_kwargs = {'user': {'read_only': True}}
+        depth = 1
+
+
+class RankingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ranking
+        fields = ['id', 'category', 'product', 'score', 'rank']
+        extra_kwargs = {'category': {'read_only': True}, 'product': {'read_only': True}}
+        depth = 1
