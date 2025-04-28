101717bb0a89e7b6c8ca024e6966643d8e62c548
Pecet13 <pecet113@gmail.com>
2025-04-28 18:01:36 +0200

    Added search bar in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import "../styles/Button.css";
 
 function Home() {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState("");
     const { isLoggedIn } = useContext(AuthContext);
 
     useEffect(() => {
@@ -24,31 +25,50 @@ function Home() {
             });
     };
 
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
-        <div>
-            {products.map((product) => (
-                <div key={product.id} className="product">
-                    <div className="product-left">
-                        <img
-                            className="product-image"
-                            src={
-                                product.image_url
-                                    ? product.image_url
-                                    : placeholder
-                            }
-                            alt={product.name}
-                        />
-                        <h2>{product.name}</h2>
+        <div className="home-wrapper">
+            <input
+                type="text"
+                className="search-bar"
+                value={search}
+                onChange={handleSearch}
+                placeholder="Search"
+            ></input>
+            <div className="product-list">
+                {filteredProducts.map((product) => (
+                    <div key={product.id} className="product">
+                        <div className="product-left">
+                            <img
+                                className="product-image"
+                                src={
+                                    product.image_url
+                                        ? product.image_url
+                                        : placeholder
+                                }
+                                alt={product.name}
+                            />
+                            <h2>{product.name}</h2>
+                        </div>
+                        <div className="product-right">
+                            {isLoggedIn && (
+                                <div>
+                                    <button className="button">
+                                        Add review
+                                    </button>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                    <div className="product-right">
-                        {isLoggedIn && (
-                            <div>
-                                <button className="button">Add review</button>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
