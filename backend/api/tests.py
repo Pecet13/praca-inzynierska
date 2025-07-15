cdbabbca06ee23eba16328df611c37961654f1c2
Pecet13 <pecet113@gmail.com>
2025-07-15 20:40:50 +0200

    First backend test working

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import ProductType, Product, Category, Comparison, Ranking
 
-# Create your tests here.
+
+class ProductTypeTestCase(TestCase):
+    def setUp(self):
+        self.product_type = ProductType.objects.create(name="Test Type")
+
+    def test_product_type_str(self):
+        self.assertEqual(str(self.product_type), "Test Type")
