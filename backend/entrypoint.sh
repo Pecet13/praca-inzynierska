a0e2d786a1182f3b9428d1a8399383f520f92a96
Pecet13 <pecet113@gmail.com>
2025-07-28 17:54:47 +0200

    Docker backend setup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if true 
+if [ ! -f .env ] 
 then
-    echo
+    cp .env.example .env
+    NEW_KEY=$(python3 -c 'import re; from django.core.management.utils import get_random_secret_key;
+print(re.escape(get_random_secret_key()))')
+    sed -i "s/^SECRET_KEY=.*$/SECRET_KEY=${NEW_KEY}/" .env
 fi
 
-python3 manage.py migrate
+python3 manage.py migrate --noinput
 python3 manage.py loaddata example_data/*.json
+python3 manage.py collectstatic --noinput
 python3 manage.py update_rankings
 
 exec "$@"
