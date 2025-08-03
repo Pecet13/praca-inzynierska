90e33730550db69335c3545c20b49e67758c314a
Pecet13 <pecet113@gmail.com>
2025-08-03 16:14:55 +0200

    Fixed static files serving on admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ProductType, Product, Category
+from .models import ProductType, Product, Category, Comparison, Ranking
 
 admin.site.register(ProductType)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Comparison)
+admin.site.register(Ranking)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -141,7 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
